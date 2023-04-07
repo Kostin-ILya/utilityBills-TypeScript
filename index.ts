6fe0942b32e9d931ecf7155ea7f4e386da4ab0e4
Ilya <lord-ilja@yandex.ru>
2023-04-07 09:12:40 +0300

    typing JS-app to TypeScript-app

--- index.ts
+++ index.ts
@@ -1,38 +1,51 @@
 const electricityUserData = {
-	readings: 95,
-	units: "kWt",
-	mode: "double",
-};
+  readings: 95,
+  units: 'kWt',
+  mode: 'double',
+}
 
 const waterUserData = {
-	readings: 3,
-	units: "m3",
-};
+  readings: 3,
+  units: 'm3',
+}
 
-const elRate = 0.45;
-const wRate = 2;
+const elRate: number = 0.45
+const wRate: number = 2
 
-const monthPayments = [0, 0]; // [electricity, water]
+const monthPayments: number[] = [0, 0] // [electricity, water]
 
-const calculatePayments = (elData, wData, elRate, wRate) => {
-	if (elData.mode === "double" && elData.readings < 50) {
-		monthPayments[0] = elData.readings * elRate * 0.7;
-	} else {
-		monthPayments[0] = elData.readings * elRate;
-	}
+const calculatePayments = (
+  { readings: elReadings, mode: elMode }: { readings: number; mode: string },
+  { readings: wReadings }: { readings: number },
+  elRate: number,
+  wRate: number
+): void => {
+  if (elMode === 'double' && elReadings < 50) {
+    monthPayments[0] = elReadings * elRate * 0.7
+  } else {
+    monthPayments[0] = elReadings * elRate
+  }
 
-	monthPayments[1] = wData.readings * wRate;
-};
+  monthPayments[1] = wReadings * wRate
+}
 
-calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+calculatePayments(electricityUserData, waterUserData, elRate, wRate)
 
-const sendInvoice = (monthPayments, electricityUserData, waterUserData) => {
-	const text = `    Hello!
-    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${monthPayments[0]}€
-    
-    This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${monthPayments[1]}€`;
+const sendInvoice = (
+  [elPayments, wPayments]: number[],
+  { readings: elReadings, units: elUnits }: { readings: number; units: string },
+  { readings: wReadings, units: wUnits }: { readings: number; units: string }
+): string => {
+  const text: string = `    Hello!
+    This month you used ${elReadings} ${elUnits} of electricity
+    It will cost: ${elPayments}€
 
-	return text;
-};
+    This month you used ${wReadings} ${wUnits} of water
+    It will cost: ${wPayments}€`
+
+  return text
+}
+
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData)
+
+console.log(invoice)
